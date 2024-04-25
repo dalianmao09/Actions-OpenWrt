79390ef3d6d0dba153f7c31d814e63caab1f3469
jackaychang <jackaychang@163.com>
2024-04-25 23:30:13 +0800

    update diy

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -19,7 +19,7 @@
 
 if [ "$1" == "LEAN" ]; then
     echo "Build From Lean wrt"
-    echo 'src-git helloworld https://github.com/fw876/helloworld;main' >>feeds.conf.default
+    echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
     rm -rf feeds/luci/themes/luci-theme-argon
     git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 fi

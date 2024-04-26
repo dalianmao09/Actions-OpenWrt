877ddadaac1022c9ff4c94478c2dd1fe0d38bd5b
jackaychang <jackaychang@163.com>
2024-04-26 09:26:43 +0800

    remove argon config

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -20,8 +20,8 @@
 if [ "$1" == "LEAN" ]; then
     echo "Build From Lean wrt"
     echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-    rm -rf feeds/luci/themes/luci-theme-argon
-    git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+#    rm -rf feeds/luci/themes/luci-theme-argon
+#    git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 fi
 
 # if [ "$1" == "ZN_COOL" ]; then

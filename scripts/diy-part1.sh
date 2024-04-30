c4613a84f4613bd9a3ac255abee0d66c958e7956
jackaychang <jackaychang@163.com>
2024-04-30 14:20:18 +0000

    update build sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -24,9 +24,7 @@ if [ "$1" == "LEAN" ]; then
 #    git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 fi
 
-# if [ "$1" == "ZN_COOL" ]; then
-#     echo "Build ZN_COOL"
-#     sed -i 's#PKG_SOURCE_URL.*#PKG_SOURCE_URL:=https://sources.immortalwrt.org/' package/lean/ipt2socks/Makefile
-#     rm -rf extra/luci/themes/luci-theme-argon
-#     git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git extra/luci/themes/luci-theme-argon
-# fi
+if [ "$1" == "IWRT" ]; then
+    echo "Build From immortalwrt"
+    echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+fi

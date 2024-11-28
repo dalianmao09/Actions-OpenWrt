4a3d30d617f1325beee37b6943e6183a01911051
jackaychang <jackaychang@163.com>
2024-11-28 15:10:14 +0800

    update sync image action

--- scripts/copy-images.sh
+++ scripts/copy-images.sh
@@ -1,12 +1,19 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 MY_REPO_BASE=registry.cn-hangzhou.aliyuncs.com/dalianmao
-imgList="$1"
-skopeo copy docker://docker.io/${imgList} docker://${MY_REPO_BASE}/${imgList}
+images="$1"
+echo "Images:[${images}]"
+IFS=','; read -ra image_array <<< "$images"
+for image in "${image_array[@]}"; do
+  image_name=`basename ${image}`
+  targetName=${MY_REPO_BASE}/${image_name}
+  echo "from [${image_name}] to [${targetName}]"
+  docker pull "${image}" --platform "linux/amd64"
+  docker tag "${image}" "${targetName}"
+  docker push  "${targetName}"
+done
+echo "Finished"
 
-# echo $imgList
-# array=(${imgList//|/ })
-# for img in ${array[@]}
-# do
-# echo "$img"
-# # 
-# done
+#src_image=${{ github.event.inputs.srcImage }}
+#dest_image=${{ github.event.inputs.destImage }}
+#echo "from ${src_image} to ${dest_image}"
+#skopeo copy docker://docker.io/${src_image} docker://registry.cn-hangzhou.aliyuncs.com/dalianmao/${dest_image}
